88bb0b4d7b30e7bf06a12ef5bf8fcf58aa91413d
unknown <santiago.premrou@gmail.com>
2023-11-17 11:23:05 -0300

    generos

--- javascript/generos.js
+++ javascript/generos.js
@@ -1,7 +1,23 @@
-let generos
 
-fetch('https://api.themoviedb.org/3/genre/movie/list?language=en&api_key=ba0b591fbb4dcbf21e7a279fceca5d5e' )
+//peliculas
+fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=ba0b591fbb4dcbf21e7a279fceca5d5e`)
 .then(function(response){
+    console.log(response)
     return response.json()
 })
-then
+
+.then(function(data){
+  
+    for(let i=0;i< data.genres.length; i++){
+    document.querySelector(".data-group").innerHTML+=
+    ` <div class="data-item">
+            <a href="./detalle-genero.html?id=${data.genres[i].id}">
+                <h3>${data.genres[i].name}</h3>
+            </a>
+        </div>`
+    }
+})
+ 
+
+
+//`https://api.themoviedb.org/3/genre/tv/list?api_key=${acaVaLaAPIKey}`
